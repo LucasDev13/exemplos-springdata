d7a739f191863fc1e5c207b3341a26745c34295e
“LucasDev13” <lucaspontes140@gmail.com>
2020-10-11 15:40:57 -0300

    Configuração do spring data e classes de entidade

---
+++ src/main/java/com/projetoSpringdata/model/Telefone.java
@@ -0,0 +1,14 @@
+package com.projetoSpringdata.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Telefone {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+}
